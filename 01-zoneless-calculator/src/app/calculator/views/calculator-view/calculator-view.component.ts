4a8eb99cbeb23887576ef00864dad48fb0c90996
Michael Rondon <mrondon72@gmail.com>
2025-01-16 00:12:28 +0100

    10. Separación de componentes

--- 01-zoneless-calculator/src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ 01-zoneless-calculator/src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -1,8 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+// Components
+import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
 @Component({
     selector: 'calculator-view',
     standalone: true,
+    imports: [
+        CalculatorComponent
+    ],
     templateUrl: './calculator-view.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
