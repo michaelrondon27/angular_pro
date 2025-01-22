30f472b81e61d042537e45d11eee6f25317935a1
Michael Rondon <mrondon72@gmail.com>
2025-01-22 23:21:08 +0100

    36. Ciclo de vida - Reinicialización

--- 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
+++ 01-zoneless-calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -11,11 +11,22 @@ describe("CalculatorService", () => {
         calculatorService = TestBed.inject(CalculatorService);
     });
 
-    it("Should be created", () => {
+    it("should be created", () => {
         expect(calculatorService).toBeTruthy();
     });
 
     it("should be created with default values", () => {
+        expect(calculatorService.lastOperator()).toBe("+");
+        expect(calculatorService.resultText()).toBe("0");
+        expect(calculatorService.subResultText()).toBe("0");
+    });
+
+    it("should set resultText, subResultText to '0' when C is pressed", () => {
+        calculatorService.lastOperator.set("*");
+        calculatorService.resultText.set("123");
+        calculatorService.subResultText.set("456");
+        calculatorService.constructNumber("C");
+
         expect(calculatorService.lastOperator()).toBe("+");
         expect(calculatorService.resultText()).toBe("0");
         expect(calculatorService.subResultText()).toBe("0");
