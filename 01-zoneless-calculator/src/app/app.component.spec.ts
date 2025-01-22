d8275d98f10da8be4179e8e4ae726bb327d82bb8
Michael Rondon <mrondon72@gmail.com>
2025-01-22 22:36:55 +0100

    33. Evaluar RouterOutlet y refactorización

--- 01-zoneless-calculator/src/app/app.component.spec.ts
+++ 01-zoneless-calculator/src/app/app.component.spec.ts
@@ -1,15 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// Components
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+    let compiled: HTMLElement;
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [AppComponent],
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        compiled = fixture.nativeElement as HTMLElement;
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
 
         expect(app).toBeTruthy();
@@ -23,21 +30,13 @@ describe('AppComponent', () => {
         expect(result).toBe(3);
     });
 
-    // it(`should have the 'zoneless-calculator' title`, () => {
-    //     const fixture = TestBed.createComponent(AppComponent);
-    //     const app = fixture.componentInstance;
+    it(`should have the 'zoneless-calculator' title`, () => {
+        const app = fixture.componentInstance;
 
-    //     expect(app.title).toEqual('zoneless-calculator');
-    // });
+        expect(app.title).toEqual('zoneless-calculator');
+    });
 
-    // it('should render title', () => {
-    //     const fixture = TestBed.createComponent(AppComponent);
-    //     fixture.detectChanges();
-
-    //     const compiled = fixture.nativeElement as HTMLElement;
-
-    //     expect(compiled.querySelector('h1')?.textContent).toContain(
-    //         'Hello, zoneless-calculator'
-    //     );
-    // });
+    it('should render router-outlet', () => {
+        expect(compiled.querySelector("router-outlet")).not.toBeNull();
+    });
 });
