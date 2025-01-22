c790725e27ae610d2319639ca3a034c6ee8ed0e4
Michael Rondon <mrondon72@gmail.com>
2025-01-22 22:53:34 +0100

    34. Evaluar clases y atributos HTML

--- 01-zoneless-calculator/src/app/app.component.spec.ts
+++ 01-zoneless-calculator/src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 // Components
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
     let compiled: HTMLElement;
     let fixture: ComponentFixture<AppComponent>;
 
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
         compiled = fixture.nativeElement as HTMLElement;
     });
 
-    it('should create the app', () => {
+    it("should create the app", () => {
         const app = fixture.componentInstance;
 
         expect(app).toBeTruthy();
@@ -30,13 +30,26 @@ describe('AppComponent', () => {
         expect(result).toBe(3);
     });
 
-    it(`should have the 'zoneless-calculator' title`, () => {
+    it(`should have the "zoneless-calculator" title`, () => {
         const app = fixture.componentInstance;
 
-        expect(app.title).toEqual('zoneless-calculator');
+        expect(app.title).toEqual("zoneless-calculator");
     });
 
-    it('should render router-outlet', () => {
+    it("should render router-outlet", () => {
         expect(compiled.querySelector("router-outlet")).not.toBeNull();
+    });
+
+    it("should render-outlet wrapped with css classes", () => {
+        const divElement: HTMLDivElement | null = compiled.querySelector("div");
+        const mustHaveClasses: string[] = "min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5".split(" ");
+
+        expect(divElement).not.toBeNull();
+
+        const divClasses: string[] = divElement?.classList.value.split(" ") ?? [];
+
+        mustHaveClasses.forEach((className: string) => {
+            expect(divClasses).toContain(className);
+        });
     });
 });
