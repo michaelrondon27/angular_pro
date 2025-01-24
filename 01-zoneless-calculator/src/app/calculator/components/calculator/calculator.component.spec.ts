44db664d9953af808446b92dedbfbc88a2aa7c38
Michael Rondon <mrondon72@gmail.com>
2025-01-24 15:13:36 +0100

    44. Mock CalculatorService

--- 01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.spec.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -3,6 +3,18 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 // Components
 import { CalculatorComponent } from "./calculator.component";
 
+// Services
+import { CalculatorService } from "@/calculator/services/calculator.service";
+
+class MockCalculatorService {
+
+    public constructNumber: jasmine.Spy<jasmine.Func> = jasmine.createSpy("constructNumber");
+    public lastOperator   : jasmine.Spy<jasmine.Func> = jasmine.createSpy("lastOperator").and.returnValue("-");
+    public resultText     : jasmine.Spy<jasmine.Func> = jasmine.createSpy("resultText").and.returnValue("100.00");
+    public subResultText  : jasmine.Spy<jasmine.Func> = jasmine.createSpy("subResultText").and.returnValue("20");
+
+}
+
 describe("CalculatorComponent", () => {
     let compiled : HTMLElement;
     let component: CalculatorComponent;
@@ -12,6 +24,9 @@ describe("CalculatorComponent", () => {
         await TestBed.configureTestingModule({
             imports: [
                 CalculatorComponent
+            ],
+            providers: [
+                { provide: CalculatorService, useClass: MockCalculatorService }
             ]
         }).compileComponents();
 
@@ -24,5 +39,11 @@ describe("CalculatorComponent", () => {
 
     it("should create the component", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should have the current getters", () => {
+        expect(component.lastOperator()).toBe("-");
+        expect(component.resultText()).toBe("100.00");
+        expect(component.subResultText()).toBe("20");
     });
 });
