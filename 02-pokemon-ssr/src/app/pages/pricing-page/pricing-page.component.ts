1edef5206ed196d62bcb8ee62db086aed361956e
Michael Rondon <mrondon72@gmail.com>
2025-01-27 14:32:56 +0100

    57. PLATFORM_ID - Cliente y Server

--- 02-pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
+++ 02-pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { ChangeDetectionStrategy, Component, OnInit, PLATFORM_ID, inject } from "@angular/core";
 import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
@@ -10,10 +11,15 @@ import { Meta, Title } from "@angular/platform-browser";
 })
 export default class PricingPageComponent implements OnInit {
 
-    private meta : Meta = inject(Meta);
-    private title: Title = inject(Title);
+    private meta      : Meta = inject(Meta);
+    private platformId: Object = inject(PLATFORM_ID);
+    private title     : Title = inject(Title);
 
     ngOnInit(): void {
+        // if (isPlatformBrowser(this.platformId)) {
+        //     document.title = "Pricing Page";
+        // }
+
         this.meta.updateTag({ content: "Este es mi Pricing Page", name: "description" });
         this.meta.updateTag({ content: "Pricing Page", name: "og:title" });
         this.meta.updateTag({ content: "HolaMundo,Michael,Rondon,Curso,Angular,PRO", name: "keywords" });
