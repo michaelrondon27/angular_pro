5a7ac3135a6e4ed503349b76283fba5817e2ac33
Michael Rondon <mrondon72@gmail.com>
2025-01-27 17:06:43 +0100

    66. Skeleton Loader

--- 02-pokemon-ssr/src/app/pages/pokemons-page/pokemons-page.component.ts
+++ 02-pokemon-ssr/src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,15 +1,27 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit, WritableSignal, signal } from "@angular/core";
 
 // Components
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
     selector: "pokemons-page",
     standalone: true,
     imports: [
-        PokemonListComponent
+        PokemonListComponent,
+        PokemonListSkeletonComponent
     ],
     templateUrl: "./pokemons-page.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class PokemonsPageComponent { }
+export default class PokemonsPageComponent implements OnInit {
+
+    // public isLoading: WritableSignal<boolean> = signal<boolean>(true);
+
+    ngOnInit(): void {
+        // setTimeout(() => {
+        //     this.isLoading.set(false);
+        // }, 5000);
+    }
+
+}
