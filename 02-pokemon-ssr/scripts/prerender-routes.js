05009a97f83df4ecaa83cee73519cbd12fa8b852
Michael Rondon <mrondon72@gmail.com>
2025-02-04 00:21:15 +0100

    84. Construir Routes.txt automáticamente

---
+++ 02-pokemon-ssr/scripts/prerender-routes.js
@@ -0,0 +1,13 @@
+const TOTAL_PAGES = 5;
+const TOTAL_POKOMENS = 10;
+
+(async () => {
+    const fs = require("fs")
+    const pages = Array.from({ length: TOTAL_PAGES }, (_, i) => i + 1);
+    const pokemonsIds = Array.from({ length: TOTAL_POKOMENS }, (_, i) => i + 1);
+
+    let fileContent = pokemonsIds.map(id => `/pokemons/${ id }`).join("\n");
+    fileContent += `\n${ pages.map(id => `/pokemons/pages/${ id }`).join("\n") }`;
+
+    fs.writeFileSync("routes.txt", fileContent);
+})();
