721002081e66634054ce443b9ae2404d7007fe17
Michael Rondon <mrondon72@gmail.com>
2025-02-18 00:43:41 +0100

    91. Component Mock

--- 02-pokemon-ssr/src/app/app.component.spec.ts
+++ 02-pokemon-ssr/src/app/app.component.spec.ts
@@ -1,22 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
 
 // Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 describe('AppComponent', () => {
-    let app    : AppComponent;
+    let app     : AppComponent;
     let compiled: HTMLDivElement;
-    let fixture: ComponentFixture<AppComponent>
+    let fixture : ComponentFixture<AppComponent>;
+
+    @Component({
+        selector: 'app-navbar',
+        standalone: true
+    })
+    class NavBarComponentMock { }
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
                 AppComponent
-            ],
-            providers: [
-                provideRouter([])
             ]
+        }).overrideComponent(AppComponent, {
+            add: {
+                imports: [
+                    NavBarComponentMock
+                ]
+            },
+            remove: {
+                imports: [
+                    NavbarComponent
+                ]
+            }
         }).compileComponents();
 
         fixture = TestBed.createComponent(AppComponent);
