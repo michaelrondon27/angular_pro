287fea71dcdcf77c3cdd0f510095eebe65463dc9
Michael Rondon <mrondon72@gmail.com>
2025-02-18 00:34:19 +0100

    90. Pruebas en AppComponent

--- 02-pokemon-ssr/src/app/app.component.spec.ts
+++ 02-pokemon-ssr/src/app/app.component.spec.ts
@@ -1,36 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
-// Componets
+// Components
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+    let app    : AppComponent;
+    let compiled: HTMLDivElement;
+    let fixture: ComponentFixture<AppComponent>
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
                 AppComponent
             ],
+            providers: [
+                provideRouter([])
+            ]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        compiled = fixture.nativeElement;
+        app = fixture.componentInstance;
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-
         expect(app).toBeTruthy();
     });
 
-    it(`should have the 'pokemon-ssr' title`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-
-        expect(app.title).toEqual('pokemon-ssr');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const compiled = fixture.nativeElement as HTMLElement;
-        fixture.detectChanges();
-
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
+    it('should render the navbar and router-outlet', () => {
+        expect(compiled.querySelector('app-navbar')).toBeTruthy();
+        expect(compiled.querySelector('router-outlet')).toBeTruthy();
     });
 });
