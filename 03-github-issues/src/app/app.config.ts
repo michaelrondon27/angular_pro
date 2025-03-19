83cf42e53f330b7a0139d26db7249371da1b8905
Michael Rondon <mrondon72@gmail.com>
2025-03-19 21:19:46 +0100

    108. TanStack Query - Instalación

--- 03-github-issues/src/app/app.config.ts
+++ 03-github-issues/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTanStackQuery, QueryClient, withDevtools } from '@tanstack/angular-query-experimental'
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideExperimentalZonelessChangeDetection(),
-        provideRouter(routes)
+        provideRouter(routes),
+        provideTanStackQuery(new QueryClient(), withDevtools())
     ]
 };
