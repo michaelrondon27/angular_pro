d55dc274947b297f3f3968929fd4f77eac477d5e
Michael Rondon <mrondon72@gmail.com>
2025-03-19 20:58:45 +0100

    107. Crear páginas y rutas

--- 03-github-issues/src/app/app.config.ts
+++ 03-github-issues/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes),
-        provideZoneChangeDetection({ eventCoalescing: true })
+        provideExperimentalZonelessChangeDetection(),
+        provideRouter(routes)
     ]
 };
