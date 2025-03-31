ec17120f9c6b011e1322bb91b44b74279b2500dc
Michael Rondon <mrondon72@gmail.com>
2025-04-01 00:30:05 +0200

    134. Pruebas - Debe de cargar labels

--- 03-github-issues/src/app/modules/issues/services/issues.service.spec.ts
+++ 03-github-issues/src/app/modules/issues/services/issues.service.spec.ts
@@ -25,4 +25,20 @@ describe('IssuesService', () => {
     it('should be created', () => {
         expect(issuesService).toBeTruthy();
     });
+
+    it('should load labels', async () => {
+        const { data } = await issuesService.labelsQuery.refetch();
+
+        expect(data?.length).toBe(30);
+
+        const [ label ] = data!;
+
+        expect(typeof label.color).toBe('string');
+        expect(typeof label.default).toBe('boolean');
+        expect(typeof label.description).toBe('string');
+        expect(typeof label.id).toBe('number');
+        expect(typeof label.name).toBe('string');
+        expect(typeof label.node_id).toBe('string');
+        expect(typeof label.url).toBe('string');
+    });
 });
