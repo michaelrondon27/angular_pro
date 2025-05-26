51de1825f6f394002dcbb2dd4a4c6ce524411590
Michael Rondon <mrondon72@gmail.com>
2025-05-26 20:45:11 +0200

    144. Crar monorepo y librearía - apx-workspace

---
+++ 05-apx-workspace/projects/apx-side-menu/src/lib/apx-side-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-apx-side-menu',
+    imports: [],
+    template: `
+        <p>
+        apx-side-menu works!
+        </p>
+    `,
+    styles: ``
+})
+export class ApxSideMenuComponent { }
